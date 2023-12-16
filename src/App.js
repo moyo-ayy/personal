1c3556c2dd6451ac84f90d729244adbc9f9e3086
moyo-ayy <moyoayoade@gmail.com>
2023-12-16 13:55:53 -0800

    first day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import headshot from './assets/MicrosoftTeams-image.png';
+import github from './assets/github.svg';
+import linkedin from './assets/linkedin.svg';
+import outlook from './assets/outlook.svg';
+import gmail from './assets/gmail.svg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='headshot-wrapper'>
+      <img src={headshot} 
+      alt=''
+      className='headshot'
+      />
+      </div>
+      <div className='Name'>
+        Moyosoreoluwa Ayoade
+      </div>
+      <div className='socials'>
+        <div className='social-wrapper'>
+          <a href='https://github.com/moyo-ayy'>
+            <img src={github} 
+            alt=''
+            />
+          </a>
+          <div>
+            Github
+          </div>
+        </div>
+
+        <div className='social-wrapper'>
+          <a href='https://www.linkedin.com/in/moyosoreoluwa-ayoade/'>
+            <img src={linkedin} 
+            alt=''
+            />
+          </a>
+          <div>
+            LinkedIn
+          </div>
+        </div>
+
+        <div className='social-wrapper'>
+          <a href='mailto:adekunmi@usf.edu'>
+            <img src={outlook} 
+            alt=''
+            />
+          </a>
+          <div>
+            Outlook
+          </div>
+        </div>
+
+        <div className='social-wrapper'>
+          <a href='mailto:moyoayoade@gmail.com'>
+            <img src={gmail} 
+            alt=''
+            />
+          </a>
+          <div>
+            Gmail
+          </div>
+        </div>
+      </div>
+
+      <div className='resume-downloader'>     
+      </div>
+
     </div>
   );
 }
